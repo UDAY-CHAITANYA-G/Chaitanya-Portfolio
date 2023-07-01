c76ae15cebab0f5a74777b1a2390668eede3323e
UDAY-CHAITANYA-G <udaychaitanyagurvindapalli@gmail.com>
2023-07-01 22:52:57 +0530

    nav-bar updated

--- src/componenets/navbar/Navbar.jsx
+++ src/componenets/navbar/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () =>{
     return(
         <div className="n-wrapper" id="home">
             <div className="n-left">
-                <div className="n-name">Chaitnaya</div>
+                <div className="n-name">Chaitanya</div>
             </div>
             <div className="n-right">
                 <div className="n-list">
                     <ul>
-                        <Link spy={true} to='home'smooth={true} activeClass="activeClass"><li>Home</li></Link>
+                        <Link spy={true} to='home' smooth={true} activeClass="activeClass"><li>Home</li></Link>
                         <Link spy={true} to='Aboutme' smooth={true} activeClass="activeClass"><li>About Me</li></Link>
                         <Link spy={true} to='Services' smooth={true} activeClass="activeClass"><li>Services</li></Link>
-                        <Link spy={true} to='Portfolio'smooth={true} activeClass="activeClass"><li>Portfolio</li></Link>
+                        <Link spy={true} to='Portfolio' smooth={true} activeClass="activeClass"><li>Portfolio</li></Link>
                         <Link spy={true} to='Blogs' smooth={true} activeClass="activeClass"><li>Blogs</li></Link>
                        
                     </ul>
